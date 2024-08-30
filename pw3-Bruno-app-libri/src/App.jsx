8d21f7de2559cafde4d40c14a9d0c615d4ea6b17
bruno3d19 <brunorodrigues16@gmail.com>
2024-08-29 22:23:33 -0300

    Sistema de navegação

--- pw3-Bruno-app-libri/src/App.jsx
+++ pw3-Bruno-app-libri/src/App.jsx
@@ -1,17 +1,43 @@
+//importa os componentes de navegação da aplicação
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
 
 // importando o MENU
 import NavBar from './components/Layout/NavBar'
 
-// import CardBooks from './components/CardBooks'
-import capaLivro from './assets/livros/cavernas_aco.jpg'
+// Importa o componente de container
+import Container from './components/Layout/Container'
+
+//importação das paginas
+import Home from './components/pages/Home'
+import ListBooks from './components/pages/ListBooks'
+import CreateBooks from './components/pages/CreateBooks'
 
 function App() {
 
   return (
     <>
+      
+      {/* Estrutura de navegação */}
 
-      <NavBar/>
+      <BrowserRouter>
+
+        <Container>
+
+          <Routes>
+
+            <Route path='/' element={<NavBar/>}>
+              <Route path='/' element={<Home/>}/>
+              <Route path='listBook' element={<ListBooks/>}/>
+              <Route path='createBook' element={<CreateBooks/>}/>
+            </Route>
+
+          </Routes>
+          
+        </Container>
+
+      </BrowserRouter>
 
     </>
   )
