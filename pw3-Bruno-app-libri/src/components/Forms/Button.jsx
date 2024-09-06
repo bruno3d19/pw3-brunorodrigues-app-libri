af69d545cc1b10ea4f71648e6f3c2d96d7f08a0e
bruno3d19 <brunorodriguesdossantos16@gmail.com>
2024-09-05 21:34:06 -0300

    adição do button, input e select

---
+++ pw3-Bruno-app-libri/src/components/Forms/Button.jsx
@@ -0,0 +1,12 @@
+import styles from './Button.module.css'
+
+function Button({rotulo}) {
+
+    return(
+        <div>
+            <button>{rotulo}</button>
+        </div>
+    )
+}
+
+export default Button
---
+++ pw3-Bruno-app-libri/src/components/Forms/Select.jsx
@@ -0,0 +1,21 @@
+import styles from './Select.module.css'
+
+function Select({name, text}) {
+    return(
+        <div className={styles.form_control}>
+
+            <label htmlFor={name}>{text}</label>
+            <select name={name} id={name}>
+                <option>Selecione uma categoria</option>
+                <option>Ficção cientifica</option>
+                <option>Fantasia heroica</option>
+                <option>Suspense</option>
+                <option>Terror</option>
+            </select>
+
+        </div>
+
+    )
+}
+
+export default Select
