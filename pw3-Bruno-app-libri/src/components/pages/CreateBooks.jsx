1fade9d48ae0894c24967ba0596546d0008d6135
bruno3d19 <brunorodriguesdossantos16@gmail.com>
2024-09-26 22:11:26 -0300

    Inserção de livro

--- pw3-Bruno-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-Bruno-app-libri/src/components/pages/CreateBooks.jsx
@@ -9,6 +9,20 @@ import Button from "../Forms/Button";
 
 const CreateBooks = ()=>{
 
+    //DEFINE O STATE DA DADOS DAS CATEGORIAS
+    const [categorias, setCategorias] = useState([])
+
+    /* STATE DE DADOS QUE VAI ARMAZENAR O OBJETO JSON DE LIVRO */
+    const [book, setBook] = useState({})
+
+    
+    /* HANDLER DE CAPTURA DOS DADOS DE INPUT (NOME DO LIVRO, AUTOR E DESCRIÇÃO) */
+    function handlerChangeBook(event) {
+        setBook({...book, [event.target.name] : event.target.value});
+        console.log(book)
+    }
+
+
     //RECUPERA OS DADOS DE CATEGORIA DA APIREST
     useEffect(()=>{
         fetch('http://localhost:5000/listagemCateorias', {
@@ -24,7 +38,8 @@ const CreateBooks = ()=>{
                 resp.json()
         ).then(
             (data)=>{
-                console.log('DATA' + data)
+                console.log('DATA: ' + data.data[3].nome_categoria)
+                setCategorias(data.data)
             }
         ).catch(
             (error)=>{
@@ -33,40 +48,82 @@ const CreateBooks = ()=>{
         )
     }, []);
 
+    /* INSERÇÃO DOS DADOS DE LIVRO */
+    function createBook(book) {
+        
+        // console.log(JSON.stringify(book))
+
+        fetch('http://localhost:5000/inserirLivro', {
+                method:'POST',
+                mode:'cors',
+                headers:{
+                'Content-Type':'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Headers':'*'
+                },
+                body: JSON.stringify(book)
+        })
+        .then(
+                    (resp)=>resp.json()
+        )
+        .then(
+                    (data)=>{
+                    console.log(data);
+                // navigate('/livros',{state:'LIVRO CADASTRADO COM SUCESSO!'});
+                }
+        )
+        .catch(
+                    (err)=>{ console.log(err) }
+        )
+    }
+
+            /* FUNÇÃO DE SUBMIT */
+                function submit(event) {
+                    event.preventDefault();
+                    createBook(book);
+            }
+
     return(
         <section className={style.create_book_container}>
 
             <h1>CADASTRO DE LIVROS</h1>
 
+            <form submit={submit}>
+
             <Input
                 type='text'
-                name='txt_livro'
+                name='nome_livro'
                 placeHolder='Digite o nome do livro aqui'
                 text='Titulo do livro'
+                handleChangeBook={handlerChangeBook}
             />
 
             <Input  
                 type='text'
-                name='txt_autor'
+                name='autor_livro'
                 placeHolder='Digite o nome do autor'
                 text='Nome do autor'
+                handleChangeBook={handlerChangeBook}
             />
 
             <Input  
                 type='text'
-                name='txt_descricao_livro'
+                name='descricao_livro'
                 placeHolder='Digite a descrição do livro'
                 text='Descrição do livro'
+                handleChangeBook={handlerChangeBook}
             />
 
             <Select
                 name='categoria'
                 text='Escolha uma categoria de livro'
+                options={categorias}
             />
 
             <Button
                 rotulo='Cadastrar Livro'
             />
+            </form>
 
         </section>
     )
